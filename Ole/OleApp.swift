f3ea2fa20efb4cf4cb25e9fc1e36ffa6bdca6240
Christopher Forbes <chrisjf@gmail.com>
2021-01-12 20:51:14 -0500

    Initial Commit

---
+++ Ole/OleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OleApp.swift
+//  Ole
+//
+//  Created by Christopher Forbes on 2021-01-12.
+//
+
+import SwiftUI
+
+@main
+struct OleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
